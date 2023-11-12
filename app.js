a7023cf49678f015c17ccafcf2de99aa4a6020f9
Isaac Johnson <isaac.johnson@gmail.com>
2023-11-12 07:45:21 -0600

    First Push

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3080;
+
+app.set('view engine', 'ejs'); // You can use a different template engine if you prefer
+
+// Define a route to render the resume page
+app.get('/resume', (req, res) => {
+  // Path to the folder containing experience JSON files
+  const experienceFolderPath = path.join(__dirname, 'experience');
+
+  // Read all experience JSON files from the folder
+  const experienceFiles = fs.readdirSync(experienceFolderPath);
+
+  // Parse each experience JSON file and store the data in an array
+  const experiences = experienceFiles.map((file) => {
+    const filePath = path.join(experienceFolderPath, file);
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  });
+
+  // Path to the folder containing extras JSON files
+  const extrasFolderPath = path.join(__dirname, 'extras');
+
+  // Read all extras JSON files from the folder
+  const extrasFiles = fs.readdirSync(extrasFolderPath);
+
+  // Parse each extras JSON file and store the data in an array
+  const extras = extrasFiles.map((file) => {
+    const filePath = path.join(extrasFolderPath, file);
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  });
+
+  // Path to the folder containing education JSON files
+  const educationFolderPath = path.join(__dirname, 'education');
+
+  // Read all education JSON files from the folder
+  const educationFiles = fs.readdirSync(educationFolderPath);
+
+  // Parse each eduucation JSON file and store the data in an array
+  const educations = educationFiles.map((file) => {
+    const filePath = path.join(educationFolderPath, file);
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  });
+
+  // Path to the folder containing contacts JSON files
+  const contactsPath = path.join(__dirname, 'contacts');
+
+  // Read all contact JSON files from the folder
+  const contactsFiles = fs.readdirSync(contactsPath);
+
+  // Parse each contacts JSON file and store the data in an array
+  const contacts = contactsFiles.map((file) => {
+    const filePath = path.join(contactsPath, file);
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  });
+
+
+  // Read name details from name.json
+  const namesPath = path.join(__dirname, 'name.json');
+  const namesData = fs.readFileSync(namesPath, 'utf-8');
+  const names = JSON.parse(namesData);
+
+
+  // Read version from version.txt
+  const versionPath = path.join(__dirname, 'version.txt');
+  const version = fs.readFileSync(versionPath, 'utf-8').trim();
+
+  // Render the resume page with the experiences, extras, contact details, and version
+  res.render('resume', { experiences, extras, educations, names, contacts, version });
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ generate-pdf.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+const { exec } = require('child_process');
+const path = require('path');
+
+// Start the Express application
+const expressProcess = exec('node app.js');
+
+expressProcess.stdout.on('data', (data) => {
+  console.log(`Express: ${data}`);
+});
+
+expressProcess.stderr.on('data', (data) => {
+  console.error(`Express Error: ${data}`);
+});
+
+expressProcess.on('close', (code) => {
+  console.log(`Express process exited with code ${code}`);
+});
+
+// Wait for the Express application to start (adjust the delay as needed)
+setTimeout(generatePDF, 5000);
+
+async function generatePDF() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Load the resume page
+  const resumeURL = 'http://localhost:3080/resume'; // Update with your actual URL
+  await page.goto(resumeURL, { waitUntil: 'networkidle0' });
+
+  // Set the PDF options
+  const pdfOptions = {
+    path: path.join(__dirname, 'output', 'resume.pdf'), // Adjust the output path as needed
+    format: 'A4',
+    printBackground: true,
+  };
+
+  // Generate PDF
+  await page.pdf(pdfOptions);
+
+  await browser.close();
+
+  // Close the Express application after generating the PDF
+  expressProcess.kill();
+}
+
+
+generatePDF();
