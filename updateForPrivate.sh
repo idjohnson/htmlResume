0114ecc3e74d2c82f643f8103c0a64492684109a
Isaac Johnson <isaac.johnson@gmail.com>
2023-11-12 12:31:53 -0600

    Update scripts and contacts json formatting (something was errant with 10)

---
+++ updateForPrivate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# For Private
+
+# Find all JSON files in subfolders
+# Specify the subfolders to search
+subfolders=("contacts" "education" "experience" "extras")
+
+for folder in "${subfolders[@]}"; do
+    # Find all JSON files in the specified subfolder
+    find "$folder" -type f -name '*.json' -print0 |
+    while IFS= read -r -d '' file; do
+        # Check if the file contains the key "tag" with the value "onlyprivate"
+        if jq -e '.tag == "onlyprivate"' "$file" > /dev/null 2>&1; then
+            # Update the key "enabled" to "true"
+            jq '.enabled = "true"' "$file" > tmpfile && mv tmpfile "$file"
+            echo "Updated $file"
+        fi
+    done
+
+    find "$folder" -type f -name '*.json' -print0 |
+    while IFS= read -r -d '' file; do
+        # Check if the file contains the key "tag" with the value "onlyprivate"
+        if jq -e '.tag == "onlypublic"' "$file" > /dev/null 2>&1; then
+            # Update the key "enabled" to "false"
+            jq '.enabled = "false"' "$file" > tmpfile && mv tmpfile "$file"
+            echo "Updated $file"
+        fi
+    done
+done
